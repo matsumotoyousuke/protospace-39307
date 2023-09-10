ba62aff2a984feaf92492fb0c8b01b743d3e4a7e
matsumotoyousuke <k_d_k_knizg@yahoo.co.jp>
2023-09-10 18:00:27 +0900

    initial commit

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,7 +1,6 @@
 class PrototypesController < ApplicationController
-  before_action :set_prototype, except: [:index, :new, :create]
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :contributor_confirmation, only: [:edit, :update, :destroy]
+  before_action :move_to_index, only: [:edit]
 
   def index
     @prototypes = Prototype.includes(:user)
@@ -21,6 +20,7 @@ class PrototypesController < ApplicationController
   end
 
   def show
+    @prototype = Prototype.find(params[:id])
     @comment = Comment.new
     @comments = @prototype.comments
   end
@@ -50,11 +50,10 @@ class PrototypesController < ApplicationController
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 
-  def set_prototype
+  def move_to_index
     @prototype = Prototype.find(params[:id])
-  end
-
-  def contributor_confirmation
-    redirect_to root_path unless current_user == @prototype.user
+    unless current_user == @prototype.user
+      redirect_to root_path 
+    end
   end
 end
