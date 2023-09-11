c2668faeb25a7660787ff62d6726009e2cdf948d
matsumotoyousuke <k_d_k_knizg@yahoo.co.jp>
2023-09-12 06:44:30 +0900

    second commit

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -29,6 +29,7 @@ class PrototypesController < ApplicationController
   end
 
   def update
+    @prototype = Prototype.find(params[:id])
     if @prototype.update(prototype_params)
       redirect_to prototype_path(@prototype)
     else
@@ -37,6 +38,7 @@ class PrototypesController < ApplicationController
   end
 
   def destroy
+    @prototype = Prototype.find(params[:id])
     if @prototype.destroy
       redirect_to root_path
     else
